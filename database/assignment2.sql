777f824dd01cae7524551000e525df5ea0c88e0b
Gabriel Puebla <gabrielpuebla16@gmail.com>
2025-05-13 21:55:25 -0300

    database folder update (sql queries)

---
+++ database/assignment2.sql
@@ -0,0 +1,33 @@
+-- Query 1
+INSERT INTO public.account(account_firstname,account_lastname,account_email,account_password)
+VALUES('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+-- Query 2
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_id = 1;
+
+-- Query 3
+DELETE FROM account
+WHERE account_id = 1;
+
+-- Query 4
+UPDATE inventory
+SET inv_description = REPLACE(inv_description,'the small interiors','a huge interior')
+WHERE inv_id = 10;
+
+-- Query 5
+SELECT
+	c. classification_name,
+	i.*
+FROM
+    inventory i
+INNER JOIN classification c
+    ON c.classification_id = i.classification_id
+WHERE classification_name = 'Sport';
+
+-- Query 6
+UPDATE inventory
+SET inv_image = REPLACE(inv_image,'/images/','/images/vehicles/'),
+	inv_thumbnail = REPLACE(inv_thumbnail,'/images/','/images/vehicles/');
+
